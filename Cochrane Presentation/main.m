68ca9e86c6127ea9526b97fbefde7b6df9ce39d6
Rayman Rosevear <Rayman@Raymans-MacBook-Pro.local>
2014-11-06 11:42:45 +0200

    Initial Commit

---
+++ Cochrane Presentation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cochrane Presentation
+//
+//  Created by Rayman Rosevear on 2014/11/06.
+//  Copyright (c) 2014 Mushroom Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
