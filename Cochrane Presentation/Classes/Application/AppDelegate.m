01d9b76d15babd34d14f3af62e357095fa41f879
Rayman Rosevear <ray@mushroomcloud.co.za>
2014-11-14 17:20:51 +0200

    Add an alert when the app starts notifying the users about the alpha status of the release and how to use the menu

--- Cochrane Presentation/Classes/Application/AppDelegate.m
+++ Cochrane Presentation/Classes/Application/AppDelegate.m
@@ -14,6 +14,8 @@
 
 @property (nonatomic, strong) CPSAppContext *applicationContext;
 
+- (void)showAlphaReleaseMessage;
+
 @end
 
 @implementation AppDelegate
@@ -24,7 +26,19 @@
     [applicationContext configureApplicationWithWindow:self.window];
 
     self.applicationContext = applicationContext;
+    
+    [self performSelector:@selector(showAlphaReleaseMessage) withObject:nil afterDelay:0.5];
+    
     return YES;
+}
+
+- (void)showAlphaReleaseMessage
+{
+    [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Alpha Release App", nil)
+                                message:NSLocalizedString(@"Welcome to the Alpha release of the Cochrane presentation app.\n\n Please note that it does not currently have much functionality. Right now it is just a skeleton containing the navigational framework, the menu, and a few placeholder content views. However, you should be able to get a feel for the look and user experience the final product will have.\n\nYou can slide the view to the right to view the menu, and tap on the menu items to navigate to the different parts of the app (which are just placeholders at the moment).", nil)
+                               delegate:nil
+                      cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                      otherButtonTitles:nil] show];
 }
 
 @end
